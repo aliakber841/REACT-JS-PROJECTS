aab16fb3d90db052cf13e845543874b8db5f5a41
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2023-09-27 09:38:17 +0500

    Add files via upload

---
+++ PROJECT 01/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first relased in 2013</li>
+            <li>Was orignally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>is maintained by Facebook</li>
+            <li>Powers thousand of enterprise apps,including mobile apps</li>
+        </ul>
+        </div>
+  )
+}
+
+export default Footer
---
+++ PROJECT 01/page.js
@@ -0,0 +1,16 @@
+"use client"
+import React, { useState } from 'react'
+import Header from '@/Components/Header'
+import Footer from '@/Components/Footer'
+
+const page = () => {
+ 
+  return (
+   <>
+   <Header />
+  <Footer />
+   </>
+  )
+}
+
+export default page
